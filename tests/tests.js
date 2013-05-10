cb87c7a7db9425d84a7fbc4d16d097189c9f67ed
David Byrd <david@byrdhou.se>
2013-05-10 17:03:23 -0400

    added test that fails on #! routes

--- tests/tests.js
+++ tests/tests.js
@@ -7,6 +7,12 @@ sink('qmark', function (test, ok) {
     ok(qmark('id') == '123', 'parses second param')
   })
 
+  test('should parse query parameter for /!#/ routes', function () {
+    window.history.pushState("test-state-2", "Tests", "/#!/blah?foo=bar&bar=foo")
+    ok(qmark('foo') == 'bar', 'parses first param')
+    ok(qmark('bar') == 'food', 'parses second param')
+  })
+
   test('should give back an object', 2, function () {
     window.history.pushState("test-state-1", "Tests", "/tests/tests.html?name=qmark&id=123")
     var queryObj = qmark();
@@ -15,7 +21,7 @@ sink('qmark', function (test, ok) {
   })
 
   test('should handle no query string case gracefully', 1, function () {
-    window.history.pushState("test-state-2", "More Tests", "/tests/tests.html")
+    window.history.pushState("test-state-3", "More Tests", "/tests/tests.html")
     ok(qmark('name') == undefined, 'handles non-existant params')
   })
 })
